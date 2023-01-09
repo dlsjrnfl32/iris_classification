3971778b01f91de1046688befc310a9972e99b50
JH LEE <dlsjrnfl@naver.com>
2023-01-09 17:02:40 +0900

    첫번째 커밋 main 생성

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""get data"""
+from sklearn.datasets import load_iris
 import pandas as pd
-df = pd.read_csv('iris.csv')
-print(df)
+import numpy as np
+
+Iris = load_iris()
+
+Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names']+['target'])
+Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
+
+X_Data = Iris_Data.iloc[:, :-1]
+Y_Data = Iris_Data.iloc[:, [-1]]
+
+"""data -> csv"""
+Iris_Data.to_csv('iris_classification.csv')
