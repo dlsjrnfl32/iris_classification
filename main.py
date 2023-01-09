32dafeb168a8a0c36c2a539b70a3349663656053
JH LEE <dlsjrnfl@naver.com>
2023-01-09 17:17:35 +0900

    세번째 커밋 Iris -> iris

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
 
-Iris = load_iris()
+iris = load_iris()
 
-Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names']+['target'])
-Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
+iris_Data = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=iris['feature_names']+['target'])
+iris_Data['target'] = iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
 
-X_Data = Iris_Data.iloc[:, :-1]
-Y_Data = Iris_Data.iloc[:, [-1]]
+X_Data = iris_Data.iloc[:, :-1]
+Y_Data = iris_Data.iloc[:, [-1]]
 
 """data -> csv"""
-Iris_Data.to_csv('iris_classification.csv')
+iris_Data.to_csv('iris_classification.csv')
